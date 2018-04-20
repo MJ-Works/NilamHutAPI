fd44a329ef71821170dc70cabc945728be2e72c9
jaggesher <jaggesher14@gmail.com>
2018-04-20 21:56:09 +0600

    Account Controller Clear

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,5 @@
 ﻿
- 
+
 using System.Security.Claims;
 using System.Threading.Tasks;
 using NilamHutAPI.Auth;
@@ -10,14 +10,14 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
- 
+
 
 namespace NilamHutAPI.Controllers
 {
     [Route("api/[controller]")]
     public class AuthController : Controller
     {
-        
+
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IJwtFactory _jwtFactory;
         private readonly JwtIssuerOptions _jwtOptions;
@@ -27,6 +27,18 @@ namespace NilamHutAPI.Controllers
             _userManager = userManager;
             _jwtFactory = jwtFactory;
             _jwtOptions = jwtOptions.Value;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Post([FromBody] RegistrationViewModel model)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded) return BadRequest(Errors.AddErrorsToModelState(result, ModelState));
+            return new OkObjectResult(new { Message = "Account Created." });
+            
         }
 
         // POST api/auth/login
@@ -43,8 +55,8 @@ namespace NilamHutAPI.Controllers
                 return BadRequest(Errors.AddErrorToModelState("login_failure", "Invalid username or password.", ModelState));
             }
 
-          var jwt = await Tokens.GenerateJwt(identity, _jwtFactory, credentials.UserName, _jwtOptions, new JsonSerializerSettings { Formatting = Formatting.Indented });
-          return new OkObjectResult(jwt);
+            var jwt = await Tokens.GenerateJwt(identity, _jwtFactory, credentials.UserName, _jwtOptions, new JsonSerializerSettings { Formatting = Formatting.Indented });
+            return new OkObjectResult(jwt);
         }
 
         private async Task<ClaimsIdentity> GetClaimsIdentity(string userName, string password)
