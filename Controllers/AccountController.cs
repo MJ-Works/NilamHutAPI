d732b0ed36b1bf5fb794c7be76dd6ce10cd95ea0
IAmMonmoy <iammonmoy@gmail.com>
2018-05-19 00:26:26 +0600

    moved login and register to same controller

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,31 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NilamHutAPI.ViewModels;
 using NilamHutAPI.Models;
 using NilamHutAPI.Data;
-using System.IdentityModel.Tokens.Jwt;
+using NilamHutAPI.Auth;
+using Microsoft.Extensions.Options;
+using NilamHutAPI.Helpers;
+using Newtonsoft.Json;
 
 namespace NilamHutAPI.Controllers
 {
-    [Route("api/[controller]/[action]")]
+    [Route("api/[controller]")]
     public class AccountController : Controller
     {
         private readonly ApplicationDbContext _appDbContext;
         private readonly UserManager<ApplicationUser> _userManager;
-        public AccountController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        private readonly IJwtFactory _jwtFactory;
+        private readonly JwtIssuerOptions _jwtOptions;
+        public AccountController(ApplicationDbContext context, UserManager<ApplicationUser> userManager,
+                                IJwtFactory jwtFactory, IOptions<JwtIssuerOptions> jwtOptions)
         {
             _appDbContext = context;
             _userManager = userManager;
+             _jwtFactory = jwtFactory;
+            _jwtOptions = jwtOptions.Value;
         }
 
-        [HttpPost]
+        [HttpPost("register")]
         public async Task<IActionResult> Post([FromBody]RegistrationViewModel model)
         {
             if (ModelState.IsValid)
@@ -39,6 +42,44 @@ namespace NilamHutAPI.Controllers
             }
 
            // return new OkObjectResult("Account created");
+        }
+
+        // POST api/auth/login
+        [HttpPost("login")]
+        public async Task<IActionResult> Post([FromBody]CredentialsViewModel credentials)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var identity = await GetClaimsIdentity(credentials.UserName, credentials.Password);
+            if (identity == null)
+            {
+                return BadRequest(Errors.AddErrorToModelState("login_failure", "Invalid username or password.", ModelState));
+            }
+
+          var jwt = await Tokens.GenerateJwt(identity, _jwtFactory, credentials.UserName, _jwtOptions, new JsonSerializerSettings { Formatting = Formatting.Indented });
+          return new OkObjectResult(jwt);
+        }
+
+        private async Task<ClaimsIdentity> GetClaimsIdentity(string userName, string password)
+        {
+            if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+                return await Task.FromResult<ClaimsIdentity>(null);
+
+            // get the user to verifty
+            var userToVerify = await _userManager.FindByNameAsync(userName);
+
+            if (userToVerify == null) return await Task.FromResult<ClaimsIdentity>(null);
+
+            // check the credentials
+            if (await _userManager.CheckPasswordAsync(userToVerify, password))
+            {
+                return await Task.FromResult(_jwtFactory.GenerateClaimsIdentity(userName, userToVerify.Id));
+            }
+
+            // Credentials are invalid, or account doesn't exist
+            return await Task.FromResult<ClaimsIdentity>(null);
         }
     }
 }
