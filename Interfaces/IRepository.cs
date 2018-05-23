96e6f89cdf541f37454806fcb266c09d7886e3d6
IAmMonmoy <iammonmoy@gmail.com>
2018-05-23 17:17:37 +0600

    added generic database functions

---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace NilamHutAPI.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> Get(int id);
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+
+        Task<int> Add(TEntity entity);
+        Task<int> AddRange(IEnumerable<TEntity> entities);
+
+        Task<int> Update(object key, TEntity entity);
+
+        Task<int> Remove(TEntity entity);
+        Task<int> RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
---
+++ Services/Repository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NilamHutAPI.Interfaces;
+using NilamHutAPI.Data;
+using NilamHutAPI.Models;
+using System.Linq;
+
+namespace NilamHutAPI.Services
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Add(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> AddRange(IEnumerable<TEntity> entities)
+        {
+            _context.Set<TEntity>().AddRange(entities);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> Update(object key, TEntity entity)
+        {
+            TEntity exist = await _context.Set<TEntity>().FindAsync(key);
+            if (exist != null)
+            {
+                _context.Entry(exist).CurrentValues.SetValues(entity);
+                await _context.SaveChangesAsync();
+            }
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _context.Set<TEntity>().Where(predicate).ToListAsync();
+        }
+
+        public async Task<TEntity> Get(int id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll()
+        {
+            return await _context.Set<TEntity>().ToListAsync(); 
+        }
+
+        public async Task<int> Remove(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> RemoveRange(IEnumerable<TEntity> entities)
+        {
+            _context.Set<TEntity>().RemoveRange(entities);
+            return await _context.SaveChangesAsync();
+        }
+     
+    }
+}
