5003995d919a10fdc509832c365df7c8f0fecc03
IAmMonmoy <iammonmoy@gmail.com>
2018-05-24 14:02:13 +0600

    Added necessary files for post

---
+++ Controllers/PostController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NilamHutAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class PostController : Controller
+    {
+    }
+}
--- ViewModels/PostRelated/PostViewModel.cs
+++ ViewModels/PostRelated/PostViewModel.cs
@@ -1,7 +1,4 @@
-using System;
-using System.ComponentModel.DataAnnotations;
-
-namespace NilamHutAPI.ViewModels
+namespace NilamHutAPI.ViewModels.PostRelated
 {
     public class PostViewModel
     {
