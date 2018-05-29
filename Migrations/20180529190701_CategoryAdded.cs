3796313d661f36f960eef3195ba9df0bd15c098d
jaggesher <jaggesher14@gmail.com>
2018-05-30 01:11:21 +0600

    New Migrations added

---
+++ Migrations/20180529190701_CategoryAdded.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NilamHutAPI.Migrations
+{
+    public partial class CategoryAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Country_CountryId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Country");
+
+            migrationBuilder.DropColumn(
+                name: "CountryId",
+                table: "User");
+
+            migrationBuilder.RenameColumn(
+                name: "CountryId",
+                table: "Products",
+                newName: "CategoryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_CountryId",
+                table: "Products",
+                newName: "IX_Products_CategoryId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CategoryName = table.Column<string>(maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Category_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Category_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "User");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "Products",
+                newName: "CountryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                newName: "IX_Products_CountryId");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CountryId",
+                table: "User",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "Country",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CountryName = table.Column<string>(maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Country", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Country_CountryId",
+                table: "Products",
+                column: "CountryId",
+                principalTable: "Country",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
