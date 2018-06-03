5478675f1b45889d7eb81a30d6afba2cb318c5ce
jaggesher <jaggesher14@gmail.com>
2018-06-04 00:02:28 +0600

    Search name min length err fixed

--- ViewModels/Shared/SearchViewModel.cs
+++ ViewModels/Shared/SearchViewModel.cs
@@ -9,7 +9,7 @@ namespace NilamHutAPI.ViewModels.Shared
         public Guid? Category { get; set; }
         public Guid? City { get; set; }
 
-        [StringLength(50, ErrorMessage = "The User SearchName must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+        [StringLength(50, ErrorMessage = "The User SearchName must be at least {2} and at max {1} characters long.", MinimumLength = 0)]
         public String searchName { get; set; }
     }
 }
